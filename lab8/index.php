8553a28df938c03cadfdc97b37134295c4573dda
K1mskii <movies2702@gmail.com>
2020-11-20 09:34:41 +0600

    upd

---
+++ lab8/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Labs</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/reset.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <main class="main">
+        
+    </main>
+
+    <script src="../js/jquery 3.5.1.min.js"></script>
+    <script src="../js/main.js"></script>
+</body>
+
+</html>
+
+
+    
